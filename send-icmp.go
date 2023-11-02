fc9438993ecc08090122587a5bb8e92b29864fdc
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-11-02 22:12:53 +0900

    update

--- send-icmp.go
+++ send-icmp.go
@@ -8,7 +8,33 @@ import (
 	"github.com/google/gopacket/pcap"
 	"log"
 	"net"
+	"strings"
 )
+
+type nwDevice struct {
+	macAddr  net.HardwareAddr
+	ipv4Addr net.IP
+}
+
+func getInterface(ifname string) nwDevice {
+	netifs, _ := net.Interfaces()
+	for _, netif := range netifs {
+		if netif.Name == ifname {
+			addrs, _ := netif.Addrs()
+			for _, addr := range addrs {
+				if !strings.Contains(addr.String(), ":") && strings.Contains(addr.String(), ".") {
+					ip, _, _ := net.ParseCIDR(addr.String())
+					return nwDevice{
+						macAddr:  netif.HardwareAddr,
+						ipv4Addr: ip,
+					}
+				}
+			}
+
+		}
+	}
+	return nwDevice{}
+}
 
 func parseMac(macaddr string) net.HardwareAddr {
 	parsedMac, _ := net.ParseMAC(macaddr)
@@ -16,28 +42,38 @@ func parseMac(macaddr string) net.HardwareAddr {
 }
 
 func main() {
+	// コマンド引数を受け取る
 	var iface = flag.String("i", "eth0", "Interface to read packets from")
+	var dstIp = flag.String("dst", "192.168.1.3", "dest ip addr")
+	var dstMac = flag.String("dstmac", "", "dest ip addr")
 	flag.Parse()
 
+	// 送信元のMACアドレスとIPアドレスを取得する
+	netif := getInterface(*iface)
+
+	// Ethernetのパケットを作成する
 	ethernet := &layers.Ethernet{
-		SrcMAC:       parseMac("00:00:00:00:00:00"),
-		DstMAC:       parseMac("00:00:00:00:00:00"),
+		SrcMAC:       netif.macAddr,
+		DstMAC:       parseMac(*dstMac),
 		EthernetType: layers.EthernetTypeIPv4,
 	}
+	// IPヘッダを作成する
 	ip := &layers.IPv4{
 		Version:  4,
 		Flags:    layers.IPv4DontFragment,
 		TTL:      64,
 		Protocol: layers.IPProtocolICMPv4,
-		SrcIP:    net.ParseIP("127.0.0.1"),
-		DstIP:    net.ParseIP("127.0.0.1"),
+		SrcIP:    netif.ipv4Addr,
+		DstIP:    net.ParseIP(*dstIp),
 	}
+	// ICMP Echo Requestのパケットを作成する
 	icmp := &layers.ICMPv4{
 		TypeCode: layers.CreateICMPv4TypeCode(layers.ICMPv4TypeEchoRequest, 0),
 		Id:       0,
 		Seq:      0,
 	}
 	packetbuf := gopacket.NewSerializeBuffer()
+	// パケットを作成する
 	err := gopacket.SerializeLayers(
 		packetbuf,
 		gopacket.SerializeOptions{
@@ -64,7 +100,7 @@ func main() {
 		icmpLayer := packet.Layer(layers.LayerTypeICMPv4)
 		reply := icmpLayer.(*layers.ICMPv4)
 		if reply.TypeCode == layers.ICMPv4TypeEchoReply {
-			fmt.Println("recieve echo reply")
+			fmt.Printf("recieve echo reply %+v\n", reply)
 			break
 		}
 	}
